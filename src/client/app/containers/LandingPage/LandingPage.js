506303da306ef1d94563f9bc7eead93e01d9d2eb
Brian <cbchien46@gmail.com>
2019-01-18 18:38:16 +0800

    Update LandingPage

--- src/client/app/containers/LandingPage/LandingPage.js
+++ src/client/app/containers/LandingPage/LandingPage.js
@@ -1,28 +1,20 @@
 import React, {Component} from 'react'
 import Heading from '../Common/heading';
 import Footer from '../common/footer';
+import SkillSelect from '../SkillSelect/SkillSelect'
 
-import Button from '../../components/Common/Button';
-import DropDownSelect from '../../components/Common/Select';
-import Input from '../../components/Common/Input';
-import SkillCard from '../../components/SkillCard/SkillCard';
-
-import {SKILL_LENGTH_OPTIONS} from './../../redux/constant/skills';
+import './LandingPage.scss'
 
 class LandingPage extends Component{
-    test = (e) => {
-        console.log('onChange', e.target.value)
-    };
-
-    test2 = (value) => {
-        console.log('select', value)
-    }
-
 	render() {
         return(
 			<div className="sunshine-landing-page">
 				<Heading />
-                Welcome to Sunshine Skills
+                <article className="sunshine-landing-page__content">
+                    <h1>Welcome to Sunshine Skills!</h1>
+                    <p>This is a board to display your skills</p>
+                    <SkillSelect />
+                </article>
 				<Footer />
 			</div>
 		)
